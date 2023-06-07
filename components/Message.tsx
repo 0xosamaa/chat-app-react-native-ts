7f5bcc7ed166a8723c7b77841b669bca0b900c4b
Osama <0xm.osamaa@gmail.com>
2023-06-08 00:16:39 +0300

    message component

---
+++ components/Message.tsx
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { IMessage, IMessageProps } from '../Models';
+
+const Message: React.FC<IMessageProps> = ({ message }) => {
+    return (
+        <View style={styles.message}>
+            <View>
+                <Text>{message.text}</Text>
+            </View>
+            <View>
+                <Text>{message.sender}</Text>
+            </View>
+            <View>
+                <Text>{message.date.toLocaleString()}</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    message: {
+        width: '40vw',
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+});
+
+export default Message;
